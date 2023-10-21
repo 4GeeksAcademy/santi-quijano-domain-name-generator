0ac7503acfb1fdf55bae6291fe2cac3b317a96e4
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-10-21 20:37:56 +0000

    Finished Project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let ext = [".com", ".net", ".us"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function domainGenerator() {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < ext.length + 1; m++) {
+          if (m < ext.length) {
+            console.log(pronoun[i] + adj[j] + noun[k] + ext[m]);
+          } else {
+            let nounHack = noun[k];
+            console.log(
+              pronoun[i] +
+                adj[j] +
+                nounHack.slice(0, -2) +
+                "." +
+                nounHack.slice(-2)
+            );
+          }
+        }
+      }
+    }
+  }
+}
+domainGenerator();
